2a1fe0bcf2ad91bc75a6e24b3ebdb773a6bedf05
K.Rohith <kallururohith@icloud.com>
2021-07-04 17:08:20 +0530

    register warning messages improved and redux dependency added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import {BrowserRouter,Route,Switch} from "react-router-dom";
+import React,{useReducer} from "react";
 import Home from "./Pages/Home";
 import './App.css';
 
+export var userContext = React.createContext();
+
+var initialState = {};
+const reducer = (state,action) => {
+    switch(action.type){
+      case "reset":
+        return initialState
+      default:
+        return state
+    }
+}
+
 function App() {
+  let [userstate,dispatch] = useReducer(reducer,initialState);
   return (
     <div className="App">
+    <userContext.Provider value={{userState:userstate,Dispatch:dispatch}}>
     <BrowserRouter basename='/proadminer'>
       <Switch>
       <Route path="/" component={Home}></Route>
       </Switch>
     </BrowserRouter>
+    </userContext.Provider>
     </div>
   );
 }
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,16 +1,21 @@
-import React,{useEffect,useRef,useState} from "react";
+import React,{useRef,useContext} from "react";
 import ReactDom from "react-dom";
+import userContext from "../../App"
 import firedb from "../../firebase"
 
 var valid = true;
+var user;
 
 function emailValidation()
 {
+  let infoRef = document.getElementById('form-info');
+  infoRef.innerHTML = '';
   var mail = document.getElementById('Email').value;
   let cmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-
+  user.Dispatch({type:"register",email: mail});
+  console.log(user);
         if(mail === ""){
-          document.getElementById('email-log').innerHTML = "Enter a valid email";
+          document.getElementById('email-log').innerHTML = "Email cannot be empty";
           valid = false;
         }
         else if (!cmail.test(mail)) {
@@ -28,7 +33,7 @@ function emailValidation()
 function passwordValidation(){
   var pwrd  = document.getElementById('Password').value;
   if(pwrd === ""){
-    document.getElementById('email-log').innerHTML = "Enter a valid email";
+    document.getElementById('password-log').innerHTML = "Password cannot be empty";
     valid = false;
   }
   else if (pwrd.match(/[a-z]/g) && pwrd.match(/[A-Z]/g) && pwrd.match(/[0-9]/g) && pwrd.match(/[^a-zA-Z\d]/g) && pwrd.length >= 6)
@@ -45,18 +50,19 @@ function passwordValidation(){
 }
 
 function cpasswordalidation(){
-
+  let infoRef = document.getElementById('form-info');
+  infoRef.innerHTML = '';
   var pwrd  = document.getElementById('Password');
   var cpwrd = document.getElementById('Cpassword');
   var cpwrdLog = document.getElementById("cpassword-log");
 
   if(cpwrd.value === ""){
-    document.getElementById('email-log').innerHTML = "Enter a valid email";
+    cpwrdLog.innerHTML = "confirmation password cannot be empty";
     valid = false;
   }else if(pwrd.value !== cpwrd.value){
         cpwrdLog.innerHTML = "Confirmation password does not match with entered password";
         valid = false;
-      }else{
+  }else{
         cpwrdLog.innerHTML = "";
         valid = true;
       }
@@ -89,24 +95,42 @@ async function Signup(){
 
 let OTP = (props) => {
   let otpRef = useRef(null);
-
+  let otpText = useRef(null);
+  let infoRef = document.getElementById('form-info');
   return (<>
-    <input ref={otpRef} id="otp" type="text" class="form-control" placeholder="Enter otp sent to your mail" aria-label="Recipient's username" aria-describedby="button-addon2"></input>
+    <input onChange={ ()=> {
+      if(otpRef.current.value !== props.otp){
+        otpText.current.innerHTML = "Incorrect otp";
+        valid = false;
+      }
+      if(otpRef.current.value === ""){
+        otpText.current.innerHTML = "otp can not be empty";
+        valid = false;
+      }
+      else{
+        otpText.current.innerHTML = "";
+        valid = true;
+      }
+    }} ref={otpRef} id="otp" type="text" class="form-control" placeholder="Enter otp sent to your mail" aria-label="Recipient's username" aria-describedby="button-addon2"></input>
 <button id="otpverify" class="btn btn-outline-secondary" type="button" id="button-addon2" 
 onClick={() => {
-  if(otpRef.current.value !== props.otp){
-    alert('Incorrect otp')
+  if(valid === false){
+    infoRef.innerHTML = 'otp does not match';
   }else{
     Signup()
   }
-}}>verify</button></>);
+}}>verify</button><div className="form-control"><p ref={otpText} className="text-danger"></p></div></>);
 
 }
 
 var Register = (props) =>{
 
+  user = useContext(userContext);
+  console.log("in register");
+  console.log(user)
   return (
     <form class="needs-validation">
+    <p id="form-info" className="text-center text-danger"></p>
     <div class="mb-3 row">
     <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
     <div class="col-sm-10">
@@ -139,14 +163,32 @@ var Register = (props) =>{
   <div id="otpblock" class="input-group mb-3">
   
   </div>
-  <button type="button" class="btn btn-info" onClick={
+  <button type="button" id="regBtn" class="btn btn-info" onClick={
   ()=>{
-    document.getElementById('Email').disabled = true;
-    document.getElementById('Password').disabled = true;
-    document.getElementById('Cpassword').disabled = true;
+    let infoRef = document.getElementById('form-info');
+    let email = document.getElementById('Email');
+    let pwrd = document.getElementById('Password');
+    let cpwrd = document.getElementById('Cpassword');
+    let regBtn = document.getElementById('regBtn');
+
+    if(email.value==="" | pwrd.value==="" | cpwrd.value===""){
+      infoRef.innerHTML = "input fields cannot be empty"
+      valid = false;
+    }
+    
+    if(valid === false){
+      infoRef.innerHTML = 'Invalid input entries';
+    }
+
+    if(valid === true){
+    email.disabled = true;
+    pwrd.disabled = true;
+    cpwrd.disabled = true;
+    regBtn.disabled = true;
     let randint = 100000 + Math.random() * (999999 - 100000);
     randint = randint.toString();
     ReactDom.render(<OTP otp={randint}></OTP>,document.getElementById('otpblock'));
+    }
   }
   }>Register</button>
     </form>);
